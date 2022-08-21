bcc889e783d68078c982243ac54a9c4c0e2bc76c
Brotherscodes <dylanbrothers@hotmail.com>
2022-08-20 21:34:53 -0400

    Updating changes to scraping,py

--- scraping.py
+++ scraping.py
@@ -1,21 +1,30 @@
-# Import Splinter and BeautifulSoup dependencies:
+# Import needed dependencies:
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
 
 # Initialize the browser, Create a data dictionary, End the WebDriver and return the scraped data:
-
 def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=True)
 
+    news_title, news_paragraph = mars_news(browser)
 
+    # Run all scraping functions and store results in a dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
 
-# Set the executable path and initialize the browser:
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
 #####################################################################################################
 
@@ -112,5 +121,7 @@ def mars_facts():
     return df.to_html()
 
 
-
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
 
