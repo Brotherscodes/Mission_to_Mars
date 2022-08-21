d17c5a7db624c2b4bd8460305c5423a1374d3f56
Brotherscodes <dylanbrothers@hotmail.com>
2022-08-20 21:26:46 -0400

    Updating scraping.py

--- scraping.py
+++ scraping.py
@@ -4,10 +4,20 @@ from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 
+# Initialize the browser, Create a data dictionary, End the WebDriver and return the scraped data:
+
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+
+
 
 # Set the executable path and initialize the browser:
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
+
+#####################################################################################################
 
 # Define functions for mars_news to allow it to function with Flask:
 
@@ -25,26 +35,16 @@ def mars_news(browser):
     html = browser.html
     news_soup = soup(html, 'html.parser')
 
-    slide_elem = news_soup.select_one('div.list_text')
-
-
-# Add a try and except error handling method to address any potential errors that may occur during 
-# web scraping:
-
+    # Add a try and except for error handling:
     try:
 
-        # Find the title and scrape it:
-        slide_elem.find('div', class_='content_title')
-
+        slide_elem = news_soup.select_one('div.list_text')
 
         # Use the parent element to find the first `a` tag and save it as `news_title`
         news_title = slide_elem.find('div', class_='content_title').get_text()
-        news_title
-
 
         # Use the parent element to find the paragraph text
         news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-        news_p
 
     except AttributeError:
         return None,None
@@ -56,9 +56,6 @@ def mars_news(browser):
 ##################################################################################################
 
 ### Featured Images from JPL:
-
-# Add a try and except error handling method to address any potential errors that may occur during 
-# web scraping:
 
 def featured_image(browser):
 
@@ -76,31 +73,34 @@ def featured_image(browser):
     html = browser.html
     img_soup = soup(html, 'html.parser')
 
+    # Add a try and except for error handling:
     try:
         # Find the relative image url
         img_rel_url = img_soup.find('img', class_='fancybox-image').get('src')
-        img_rel_url
+
     except AttributeError:
+
         return None
 
 
     # Use the base URL to create an absolute URL
     img_url = f'https://spaceimages-mars.com/{img_rel_url}'
-    img_url
 
     return img_url
-# Facts about Planets table scrape:
 
+#####################################################################################################
+
+
+# Facts about Planets table scrape:
 ### Scraping full table with Pandas into a DataFrame:
 
-
-
-# Add a try and except error handling method to address any potential errors that may occur during 
-# web scraping:
-
 def mars_facts():
+
+    # Add a try and except for error handling:
     try:
+
         df = pd.read_html('https://galaxyfacts-mars.com')[0]
+
     except BaseException:
         return None
 
